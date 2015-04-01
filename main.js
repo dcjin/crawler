039d9519cbe90626cbd6c315fa457844092df7f8
ws <dcjin@Ctrip.com>
2015-04-01 17:49:32 +0800

    细分info，修改数据库表结构

--- main.js
+++ main.js
@@ -4,38 +4,9 @@ var parse = require('./parse'),
 
 var jobInfo = parse.getInfo();
 
-// jobInfo.forEach(function (item) {
-// 	storage.storeInfo(item);
-// })
-
-// async.series([
-// 	function (callback) {
-// 		jobInfo = parse.getInfo();
-// 		callback(null);
-// 	},
-// 	function (callback) {
-// 		// jobInfo.forEach(function (item) {
-// 		// 	storage.storeInfo(item);
-// 		// });
-// 		// callback(null);
-// 		console.log(jobInfo);
-// 	}], function (err) {
-// 	if (err) throw err;
-// 	console.log('完成');
-// });
 setTimeout(function () {
 	jobInfo.forEach(function (info) {
-		storage.storeInfo(info);
 		//console.log(info);
-		// var sql = 'INSERT INTO jobInfo (name, company, address, time) values(' + info.job + ','+ info.company +',' + info.address + ', ' + info.time + ')';
-		// console.log(sql);
+		storage.storeInfo(info);
 	});
-
-	console.log('完成');
-	// storage.storeInfo({
-	// 	job:'实习',
-	// 	company: '杭州行行行想公司',
-	// 	address: '杭州',
-	// 	time: '2015-3-31'
-	// })
 }, 3000);
--- storage.js
+++ storage.js
@@ -1,5 +1,4 @@
-var mysql = require('mysql'),
-	async = require('async');
+var mysql = require('mysql');
 
 //创建连接池
 var pool = mysql.createPool({
@@ -10,11 +9,24 @@ var pool = mysql.createPool({
 	connectionLimit: 15
 });
 
-exports.storeInfo = function (info) {
+exports.storeInfo = function (item) {
 	pool.getConnection(function (err, conn) {
 		if (err) { console.log('err: ' + err); }
 
-		var sql = "INSERT INTO jobInfo (name, company, address, time) values('" + info.job + "','" + info.company + "','" + info.address + "','" + info.time + "')";
+		var REQUIRED_PARAMETER = ['jobID', 'job', 'company', 'address', 'time', 'degree', 'experience', 'companyNature', 'companySize', 'introduce'];
+
+		REQUIRED_PARAMETER.forEach(function (param) {
+			if (typeof item[param] === 'undefined') {
+				if (param === 'jobID') {
+					item[param] = 0;
+				}
+				item[param] = '';
+			}
+		});
+
+		var sql = "INSERT INTO jobInfo (id, name, company, address, time, degree, experience, companyNature, companySize, introduce) values('"
+			+ item.jobID + "','" + item.job + "','" + item.company + "','" + item.address + "','" + item.time + "','" + item.degree + "','" + item.experience + "','"
+			+ item.companyNature + "','" + item.companySize + "','" + item.introduce +"')";
 
 		conn.query(sql, function(err, results) {
 			if (err) { console.log(err); return; }
