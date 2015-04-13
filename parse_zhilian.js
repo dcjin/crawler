63e2955cd00ca32b03c9e445bc82fb030bd9a38f
ws <dcjin@Ctrip.com>
2015-04-13 21:02:53 +0800

    初试正则

--- parse_zhilian.js
+++ parse_zhilian.js
@@ -7,7 +7,6 @@ var request = require('request'),
 	cheerio = require('cheerio'),
 	iconv = require('iconv-lite');
 
-//杭州 - 实习
 var page = 1;
 var url = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=5006000&sj=299%3b302%3b301%3b381&jl=653&sm=0&sg=de3ab3dfb3f04814ab8bd6d4401349cf&p=' + page;
 
@@ -17,19 +16,14 @@ var all = [];
 request({url: url, encoding: 'UTF-8'}, function (err, res, body) {
 	if (!err && res.statusCode === 200) {
 		var $ = cheerio.load(body);
-		//一条table就是一条完整的信息
-		var a = $('table[class=newlist]');
-		a.each(function (index, ele) {
+		var aContent = $('#newlist_list_content_table').find('.newlist');
+		aContent.each(function (index, ele) {
 			if(index !== 0){
 				var name = $(ele).find('.zwmc').find('a').eq(0).text();
 				var a = $(ele).find('.newlist_deatil_two').find('span').eq(1).text();
 				all.push(a);
 			}
 		});
-		console.log(all);
+		console.log($(content).length);
 	}
 })
-
-function getInfo() {
-	//
-}
