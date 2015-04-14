40bff582f3a93541f9fa7fe1856706aab5495d65
dcjin <dcjin@Ctrip.com>
2015-04-14 22:00:56 +0800

    add regexp

--- parse_zhilian.js
+++ parse_zhilian.js
@@ -5,8 +5,11 @@
 
 var request = require('request'),
 	cheerio = require('cheerio'),
-	iconv = require('iconv-lite');
+	iconv = require('iconv-lite'),
+	settings = require('./settings');
 
+var baseReg = settings.all.reg_for_zhilian.baseReg,
+	detailReg = settings.all.reg_for_zhilian.detailReg;
 var page = 1;
 var url = 'http://sou.zhaopin.com/jobs/searchresult.ashx?bj=5006000&sj=299%3b302%3b301%3b381&jl=653&sm=0&sg=de3ab3dfb3f04814ab8bd6d4401349cf&p=' + page;
 
@@ -15,15 +18,19 @@ iconv.extendNodeEncodings();
 var all = [];
 request({url: url, encoding: 'UTF-8'}, function (err, res, body) {
 	if (!err && res.statusCode === 200) {
-		var $ = cheerio.load(body);
-		var aContent = $('#newlist_list_content_table').find('.newlist');
+		var $ = cheerio.load(body, {decodeEntities: false});
+		var aContent = $('.newlist');
 		aContent.each(function (index, ele) {
 			if(index !== 0){
-				var name = $(ele).find('.zwmc').find('a').eq(0).text();
-				var a = $(ele).find('.newlist_deatil_two').find('span').eq(1).text();
-				all.push(a);
+				// var name = $(ele).find('.zwmc').find('a').eq(0).text();
+				// var a = $(ele).find('.newlist_deatil_two').find('span').eq(1).text();
+				// all.push(a);
+				var result = baseReg.exec($(ele).html());
+				if(result) {
+					console.log(result[2]);
+				}
 			}
 		});
-		console.log($(content).length);
+		//console.log(a[1]);
 	}
 })
---
+++ settings.js
@@ -0,0 +1,17 @@
+exports.all = {
+	//regexp for zhilian
+	reg_for_zhilian: {
+		baseReg: /.*<td class="zwmc">\s*<input.*value="(.*)" onclick=.*>\s*<div.*>\s*<a.*>(.*)<img.*>\s*<\/a>\s*<\/div>\s*<\/td>\s*<td class="gsmc">\s*<a.*href="(.*)" target=.*>(.*)<\/a>\s*<\/td>\s*<td class="zwyx">(.*)<\/td>\s*<td class="gzdd">(.*)<\/td>\s*<td class="gxsj">\s*<span>(.*)<\/span>.*<\/td>.*/,
+		detailReg: /.*<li class="newlist_deatil_two">\s*<span>(.*)<\/span>\s*<span>(.*)<\/span>\s*<span>(.*)<\/span>\s*<span>(.*)<\/span>\s*<span>(.*)<\/span>\s*<\/li>\s*<li class="newlist_deatil_last">(.*)<\/li>.*/
+	},
+
+	//regexp for 51
+	reg_for_51: {
+
+	},
+
+	//regexp for ganji
+	reg_for_ganji: {
+
+	}
+}
